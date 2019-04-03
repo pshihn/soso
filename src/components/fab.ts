6cd0a56d2a417a8c95767391f47ff1cef0fe1d68
Preet Shihn <preetshihn@gmail.com>
2019-04-03 15:02:13 -0700

    .

--- src/components/fab.ts
+++ src/components/fab.ts
@@ -23,11 +23,11 @@ export class SosoIconButton extends LitElement {
       color: inherit;
       user-select: none;
       position: relative;
-      box-shadow: 0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12);
+      box-shadow: var(--soso-fab-shadow, 0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12));
       transition: box-shadow 0.28s ease;
     }
     button:hover,button:focus {
-      box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12);
+      box-shadow: var(--soso-fab-focus-shadow, 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12));
     }
     button::before {
       content: '';
