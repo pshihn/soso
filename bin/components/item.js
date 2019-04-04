98b396c0d8d0a37688a8a7897f1110206ad56881
Preet Shihn <preetshihn@gmail.com>
2019-04-04 16:36:08 -0700

    list control

---
+++ bin/components/item.js
@@ -0,0 +1,95 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { LitElement, html, customElement, property, css } from 'lit-element';
+let SosoItem = class SosoItem extends LitElement {
+    constructor() {
+        super(...arguments);
+        this.value = '';
+        this.name = '';
+        this.selected = false;
+    }
+    static get styles() {
+        return css `
+    :host {
+      display: inline-block;
+      font-size: 14px;
+      text-align: left;
+    }
+    button {
+      cursor: pointer;
+      outline: none;
+      overflow: hidden;
+      color: inherit;
+      user-select: none;
+      position: relative;
+      font-family: inherit;
+      text-align: inherit;
+      font-size: inherit;
+      letter-spacing: 1.25px;
+      padding: 1px 10px;
+      min-height: 36px;
+      text-transform: inherit;
+      background: none;
+      border: none;
+      transition: background-color 0.3s ease, color 0.3s ease;
+      width: 100%;
+      box-sizing: border-box;
+    }
+    button.selected {
+      background: var(--soso-highlight-color, #018786);
+      color: var(--soso-highlight-foreground, white);
+    }
+    button::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: currentColor;
+      opacity: 0;
+    }
+    button:hover::before {
+      opacity: 0.05;
+    }
+    button span {
+      display: inline-block;
+      transition: transform 0.2s ease;
+    }
+    button:active span {
+      transform: scale(1.02);
+    }
+    `;
+    }
+    render() {
+        return html `
+    <button class="${this.selected ? 'selected' : ''}">
+      <span>
+        <slot></slot>
+      </span>
+    </button>`;
+    }
+};
+__decorate([
+    property(),
+    __metadata("design:type", Object)
+], SosoItem.prototype, "value", void 0);
+__decorate([
+    property(),
+    __metadata("design:type", Object)
+], SosoItem.prototype, "name", void 0);
+__decorate([
+    property({ type: Boolean }),
+    __metadata("design:type", Object)
+], SosoItem.prototype, "selected", void 0);
+SosoItem = __decorate([
+    customElement('soso-item')
+], SosoItem);
+export { SosoItem };
