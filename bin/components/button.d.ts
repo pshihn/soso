c5fe393f2493c1a6a3f6ea568ff422344f9be5f9
Preet Shihn <preetshihn@gmail.com>
2019-04-17 12:31:59 -0700

    .

--- bin/components/button.d.ts
+++ bin/components/button.d.ts
@@ -1,8 +1,9 @@
-import { LitElement, TemplateResult, CSSResult } from 'lit-element';
+import { LitElement, TemplateResult, CSSResult, PropertyValues } from 'lit-element';
 export declare class SosoButton extends LitElement {
     outlined: boolean;
     solid: boolean;
     disabled: boolean;
     static readonly styles: CSSResult;
     render(): TemplateResult;
+    updated(changed: PropertyValues): void;
 }
