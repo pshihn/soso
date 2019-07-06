4b81e40ba1d2c38f0188c2953d153d5f4981b769
Preet Shihn <preetshihn@gmail.com>
2019-07-06 12:06:09 -0700

    .

--- bin/components/app-shell-old.d.ts
+++ bin/components/app-shell-old.d.ts
@@ -1,11 +1,13 @@
-import { LitElement, TemplateResult, CSSResult } from 'lit-element';
-export declare class SososAppShell extends LitElement {
+import { LitElement, TemplateResult, CSSResultArray } from 'lit-element';
+export declare class SosoAppShell extends LitElement {
     drawerOpen: boolean;
+    disableDrawer: boolean;
     private resizeListener;
-    static readonly styles: CSSResult;
+    static readonly styles: CSSResultArray;
     render(): TemplateResult;
     connectedCallback(): void;
     disconnectedCallback(): void;
+    updated(): void;
     private toggleDrawer;
     private closeDrawer;
     private onResize;
