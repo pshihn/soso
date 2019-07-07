06fb9cf6138f46e42fecb5ee8454c3ac224616c8
Preet Shihn <preetshihn@gmail.com>
2019-07-07 13:46:53 -0700

    .

---
+++ bin/components/dialog-view.d.ts
@@ -0,0 +1,6 @@
+import { LitElement, TemplateResult, CSSResult } from 'lit-element';
+export declare class SosoDialogView extends LitElement {
+    label: string;
+    static readonly styles: CSSResult;
+    render(): TemplateResult;
+}
---
+++ src/components/dialog-view.ts
@@ -0,0 +1,54 @@
+import { LitElement, html, TemplateResult, customElement, property, css, CSSResult } from 'lit-element';
+
+@customElement('soso-dialog-view')
+export class SosoDialogView extends LitElement {
+  @property() label = '';
+
+  static get styles(): CSSResult {
+    return css`
+    :host {
+      display: block;
+      max-width: 400px;
+      margin: 0 auto;
+      box-sizing: border-box;
+      background: white;
+      border-radius: 3px;
+      overflow: hidden;
+      box-shadow: 0 11px 15px -7px rgba(0,0,0,.2), 0 24px 38px 3px rgba(0,0,0,.14), 0 9px 46px 8px rgba(0,0,0,.12);
+    }
+    #toolbar {
+      padding: 16px;
+      text-transform: capitalize;
+      text-align: var(--soso-dialog-title-align, left);
+      background: var(--soso-dialog-title-bg, #f5f5f5);
+      color: var(--soso-dialog-title-color, #000000);
+      border-bottom: var(--soso-dialog-title-border, none);
+      letter-spacing: 1px;
+    }
+    #content {
+      padding: 0 16px;
+    }
+    #footer {
+      padding: 16px;
+      text-align: var(--soso-dialog-footer-align, right);
+      background: var(--soso-dialog-footer-bg, #f5f5f5);
+      border-top: var(--soso-dialog-footer-border, none);
+    }
+    #footer ::slotted(*) {
+      margin-left: 8px;
+    }
+    `;
+  }
+
+  render(): TemplateResult {
+    return html`
+    <div id="toolbar">${this.label}</div>
+    <div id="content">
+      <slot name="main"></slot>
+    </div>
+    <div id="footer">
+      <slot name="footer"></slot>
+    </div>
+    `;
+  }
+}
