cbeecf6e3f512daae293fad2b718fb1c9110dd81
Preet Shihn <preetshihn@gmail.com>
2019-08-08 13:24:55 -0700

    grouped box

---
+++ bin/components/group.js
@@ -0,0 +1,135 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { LitElement, html, customElement, css, query, property } from 'lit-element';
+import { flex } from '../styles/flex';
+let SosoGroup = class SosoGroup extends LitElement {
+    constructor() {
+        super(...arguments);
+        this.label = '';
+    }
+    static get styles() {
+        return [
+            flex,
+            css `
+        :host {
+          display: inline-block;
+          box-sizing: border-box;
+          color: #000;
+          --soso-border-color: rgba(0,0,0,0.24);
+          --soso-text-input-highlight: var(--soso-highlight-color, #6200ee);
+          --soso-text-input-border: 1px solid;
+        }
+        #container {
+          position: relative;
+          box-sizing: border-box;
+          padding: 12px 16px 14px;
+          outline: none;
+        }
+        #overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          pointer-events: none;
+        }
+        #leftOverlay {
+          width: 12px;
+          border: var(--soso-text-input-border);
+          border-radius: 4px 0 0 4px;
+          border-right: none;
+          border-color: var(--soso-border-color);
+          box-sizing: border-box;
+        }
+        #rightOverlay {
+          border: var(--soso-text-input-border);
+          border-radius: 0 4px 4px 0;
+          border-left: none;
+          border-color: var(--soso-border-color);
+          box-sizing: border-box;
+        }
+        #midOverlay {
+          border: var(--soso-text-input-border);
+          border-left: none;
+          border-right: none;
+          border-top: none;
+          position: relative;
+          border-color: var(--soso-border-color);
+          padding-right: 8px;
+        }
+        #midOverlay.empty {
+          padding-right: 0;
+        }
+        #midOverlay span {
+          opacity: 0;
+          font-size: 0.75rem;
+          white-space: nowrap;
+        }
+        label {
+          position: absolute;
+          left: 0;
+          top: 17px;
+          opacity: 0.6;
+          font-size: 1rem;
+          line-height: 1;
+          transform: translateX(-4px) translateY(-26px) scale(0.75);
+          transition: transform .15s cubic-bezier(.4,0,.2,1);
+          white-space: nowrap;
+        }
+
+        @media (hover: hover) {
+          #container:hover {
+            --soso-border-color: rgba(0,0,0,0.65);
+          }
+          #container.focussed {
+            --soso-border-color: var(--soso-text-input-highlight, #000);
+            --soso-text-input-border: 2px solid;
+            color: var(--soso-border-color);
+          }
+        }
+      `
+        ];
+    }
+    render() {
+        const midOverlayClass = (this.label || '').trim() ? '' : 'empty';
+        return html `
+    <div id="container" tabindex="0" @focus="${this.onFocus}" @click="${this.onFocus}" @blur="${this.onBlur}">
+      <slot></slot>
+      <div id="overlay" class="horizontal layout">
+        <div id="leftOverlay"></div>
+        <div id="midOverlay" class="${midOverlayClass}">
+          <span>${this.label}</span>
+          <label>${this.label}</label>
+        </div>
+        <div id="rightOverlay" class="flex"></div>
+      </div>
+    </div>
+    `;
+    }
+    onFocus() {
+        this.container.classList.add('focussed');
+    }
+    onBlur() {
+        this.container.classList.remove('focussed');
+    }
+};
+__decorate([
+    property(),
+    __metadata("design:type", Object)
+], SosoGroup.prototype, "label", void 0);
+__decorate([
+    query('#container'),
+    __metadata("design:type", HTMLDivElement)
+], SosoGroup.prototype, "container", void 0);
+SosoGroup = __decorate([
+    customElement('soso-group')
+], SosoGroup);
+export { SosoGroup };
---
+++ src/components/group.ts
@@ -0,0 +1,118 @@
+import { LitElement, html, TemplateResult, customElement, css, CSSResultArray, query, property } from 'lit-element';
+import { flex } from '../styles/flex';
+
+@customElement('soso-group')
+export class SosoGroup extends LitElement {
+  @property() label = '';
+  @query('#container') private container?: HTMLDivElement;
+
+  static get styles(): CSSResultArray {
+    return [
+      flex,
+      css`
+        :host {
+          display: inline-block;
+          box-sizing: border-box;
+          color: #000;
+          --soso-border-color: rgba(0,0,0,0.24);
+          --soso-text-input-highlight: var(--soso-highlight-color, #6200ee);
+          --soso-text-input-border: 1px solid;
+        }
+        #container {
+          position: relative;
+          box-sizing: border-box;
+          padding: 12px 16px 14px;
+          outline: none;
+        }
+        #overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          pointer-events: none;
+        }
+        #leftOverlay {
+          width: 12px;
+          border: var(--soso-text-input-border);
+          border-radius: 4px 0 0 4px;
+          border-right: none;
+          border-color: var(--soso-border-color);
+          box-sizing: border-box;
+        }
+        #rightOverlay {
+          border: var(--soso-text-input-border);
+          border-radius: 0 4px 4px 0;
+          border-left: none;
+          border-color: var(--soso-border-color);
+          box-sizing: border-box;
+        }
+        #midOverlay {
+          border: var(--soso-text-input-border);
+          border-left: none;
+          border-right: none;
+          border-top: none;
+          position: relative;
+          border-color: var(--soso-border-color);
+          padding-right: 8px;
+        }
+        #midOverlay.empty {
+          padding-right: 0;
+        }
+        #midOverlay span {
+          opacity: 0;
+          font-size: 0.75rem;
+          white-space: nowrap;
+        }
+        label {
+          position: absolute;
+          left: 0;
+          top: 17px;
+          opacity: 0.6;
+          font-size: 1rem;
+          line-height: 1;
+          transform: translateX(-4px) translateY(-26px) scale(0.75);
+          transition: transform .15s cubic-bezier(.4,0,.2,1);
+          white-space: nowrap;
+        }
+
+        @media (hover: hover) {
+          #container:hover {
+            --soso-border-color: rgba(0,0,0,0.65);
+          }
+          #container.focussed {
+            --soso-border-color: var(--soso-text-input-highlight, #000);
+            --soso-text-input-border: 2px solid;
+            color: var(--soso-border-color);
+          }
+        }
+      `
+    ];
+  }
+
+  render(): TemplateResult {
+    const midOverlayClass = (this.label || '').trim() ? '' : 'empty';
+    return html`
+    <div id="container" tabindex="0" @focus="${this.onFocus}" @click="${this.onFocus}" @blur="${this.onBlur}">
+      <slot></slot>
+      <div id="overlay" class="horizontal layout">
+        <div id="leftOverlay"></div>
+        <div id="midOverlay" class="${midOverlayClass}">
+          <span>${this.label}</span>
+          <label>${this.label}</label>
+        </div>
+        <div id="rightOverlay" class="flex"></div>
+      </div>
+    </div>
+    `;
+  }
+
+  private onFocus() {
+    this.container!.classList.add('focussed');
+  }
+
+  private onBlur() {
+    this.container!.classList.remove('focussed');
+  }
+}
